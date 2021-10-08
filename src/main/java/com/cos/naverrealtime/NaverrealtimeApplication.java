1b5383e08f29614a6dcf1e9b29f1dcf2a2c23152
unknown <bark6256@naver.com>
2021-10-08 17:33:10 +0900

    yml 설정

---
+++ src/main/java/com/cos/naverrealtime/NaverrealtimeApplication.java
@@ -0,0 +1,13 @@
+package com.cos.naverrealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverrealtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverrealtimeApplication.class, args);
+	}
+
+}
